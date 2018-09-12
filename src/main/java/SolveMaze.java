7220d1242c12aa9f23f198762c21c779426665e8
smzeng <stacymz2@illinois.edu>
2018-09-12 12:33:02 -0500

    solved it! bro ya need to exit your loop yourself friend

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,21 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            maze.turnRight();
+            if (maze.canMove()){
+                maze.move();
+            } else {
+                maze.turnLeft();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnLeft();
+                    maze.move();
+                }
+            }
+            if (maze.isFinished()) {
+                break;
+            }
         }
 
         if (maze.isFinished()) {
